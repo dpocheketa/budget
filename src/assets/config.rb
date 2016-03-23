1e5b0ec1e828736ff4fd650c2c865f45509c63f1
dpocheketa <dpocheketa@gmail.com>
2016-03-23 22:44:02 +0200

    add config.rb

---
+++ src/assets/config.rb
@@ -0,0 +1,16 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+output_style = :compressed
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
