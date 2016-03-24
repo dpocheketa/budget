b699f9200db5996abb6898843d3123944ae69e0d
dpocheketa <dpocheketa@gmail.com>
2016-03-24 20:24:50 +0200

    change server port

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', routes);
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
 
   const {address, port} = server.address();
 
