5ee87b60b06a80f30b733970d8b7815cacea79bd
dpocheketa <dpocheketa@gmail.com>
2016-03-29 02:14:46 +0300

    handle not matched emails

--- src/modules/auth/signup/signUpController.js
+++ src/modules/auth/signup/signUpController.js
@@ -3,23 +3,29 @@
 import BasicClass from 'lib/basicClass.js';
 
 export default class SignUpController extends BasicClass{
-	constructor(userService, $state){
+	constructor(userService, $state, $scope){
     super(arguments);
 	}
 
   signUp(){
-    if (this.signUpForm.$valid && _.isEqual(this.userPassword, this.userRepeatPassword)) {
-      this.userService.signUp({
-        username: this.user.username,
-        password: this.user.password
-      }).then((response)=>{
-        this.$state.go('app.dashboard');
-      }).catch((error)=>{
-        this.errorMessage = error.message;
-        this.error = true;
-        this.$scope.$apply();
-      });
+    if (this.signUpForm.$valid && _.isEqual(this.user.password, this.user.repeatPassword)) {
+      this.proceedSignUp();
+    } else {
+      this.error = true;
+      this.errorMessage = 'Your passwords do not match';
     }
   }
 
+  proceedSignUp(){
+    this.userService.signUp({
+      username: this.user.username,
+      password: this.user.password
+    }).then((response)=>{
+      this.$state.go('app.dashboard');
+    }).catch((error)=>{
+      this.errorMessage = error.message;
+      this.error = true;
+      this.$scope.$apply();
+    });
+  }
 }
